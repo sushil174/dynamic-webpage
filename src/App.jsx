dd2bd3a1b8f348be3438950cc93b74ac557fdcb9
sushil174 <sushilkoregave123@gmail.com>
2024-11-23 01:08:27 +0530

    add loader function

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,37 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import EditPage from './components/EditPage'
 import RenderPage from './components/RenderPage'
-
+import { getPages, getPageById } from '../api'
 const router = createBrowserRouter([
   {
     path : '/',
-    element : <HomePage />
+    element : <HomePage />,
+    loader: async () => {
+      const pages = await getPages();
+      return (pages)
+    }
   }, 
 
   {
     path : '/edit/:id',
     element : <EditPage />,
+    loader: async ({params}) => {
+      if(params.id === "new") return {page : null}
+      const page = await getPageById(params.id);
+      return {page}
+    }
   },
 
   {
     path : '/view/:id',
-    element : <RenderPage />
+    element : <RenderPage />,
+    loader: async ({params}) => {
+      const page = await getPageById(params.id);
+      return {page}
+    }
   }
 ])
+
 function App() {
   return <RouterProvider router={router} />
 }
