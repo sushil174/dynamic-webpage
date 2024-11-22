6e90015d013186c16ab58d2d1a5854d40c2bcbd4
sushil174 <sushilkoregave123@gmail.com>
2024-11-23 01:03:50 +0530

    api functions

---
+++ api.js
@@ -0,0 +1,33 @@
+const API_URL = 'http://localhost:5000/api/pages';
+
+export const getPages = async () => {
+    const response = await fetch(API_URL)
+    return response.json()
+}
+
+export const getPageById = async (id) => {
+    const response = await fetch(`${API_URL}/${id}`)
+    return response.json()
+}
+
+export const createPage = async (pageData) => {
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(pageData)
+    })
+    return response.json();
+}
+
+export const updatePage = async (id, pageData) => {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method:'PUT',
+        headers: {'Content-Type':'application.json'},
+        body:JSON.stringify(pageData)
+    })
+    return response.json()
+}
+
+export const deletePage = async (id) => {
+    await fetch(`${API_URL}/${id}`, {method:'DELETE'})
+}
--- src/components/EditPage.jsx
+++ src/components/EditPage.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 export default function EditPage() {
-    return (
-        <h1>EditPage</h1>
-    )
+    const page = {id:2, title:"hello", content:"nice to meet you",image:"nothing for now", published:false}
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState(page || {
+        title: '', content: '', image: '', published: false
+    })
+
+    const handleInputChange = e => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]:value})
+    }
+
+    const handleSubmit = async () => {
+        if(!page) {
+            
+        }
+    }
 }
