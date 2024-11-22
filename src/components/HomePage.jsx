bc9a7614b53002add07700d3c62e84e960b621f2
sushil174 <sushilkoregave123@gmail.com>
2024-11-22 22:21:54 +0530

    homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,25 @@
+import {Link} from 'react-router-dom'
+
 export default function HomePage() {
+    const pages = [
+        {
+            id : 1,
+            title : "testing...."
+        }
+    ]
     return (
-        <h1>HomePage</h1>
-    )
+        <div>
+            <h1>My Pages</h1>
+            <Link to="/edit/new">Create New Page</Link>
+            <div>
+                {pages.map((page) => (
+                    <div key={page.id}>
+                        <h2>{page.title}</h2>
+                        <Link to={`/view/${page.id}`}>View</Link>
+                        <Link to={`/edit/${page.id}`}>Edit</Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
